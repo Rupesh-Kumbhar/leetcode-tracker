1e3f4b41ba1b1212f076bf5389bca09f75c63b68
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-04-19 09:52:34 +0530

    Added home page

---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './home.scss';
+
+function Home(){
+    return(
+        <div className='home'>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the home page of our application.</p>
+        </div>
+    );
+}
+
+export default Home;
