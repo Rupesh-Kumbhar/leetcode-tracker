324fa3ca39bfd4b646c50f7ac24304cec0a60177
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-04-22 08:46:59 +0530

    Added table

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -7,7 +7,9 @@ function Home(){
     return (
       <div className="col-sm-12 ">
         <div className="home py-5">
+          <a href='https://www.geeksforgeeks.org/dsa/logic-building-problems/'>
           <h1>Leetcode Tracker</h1>
+            </a>
         </div>
         <ProblemTable />
 
